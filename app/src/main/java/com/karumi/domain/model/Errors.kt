dbc05e26166d7f13ee01e763532110dd7c7df28a
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-10-29 10:23:59 +0100

    remove unnecessary code

--- app/src/main/java/com/karumi/domain/model/Errors.kt
+++ app/src/main/java/com/karumi/domain/model/Errors.kt
@@ -1,7 +1,6 @@
 package com.karumi.domain.model
 
 sealed class DomainError
-class NotInternetDomainError() : DomainError()
-data class UnknownDomainError(val errorMessage: String = "Unknow Error") : DomainError()
+class NotInternetDomainError : DomainError()
+data class UnknownDomainError(val errorMessage: String = "Unknown Error") : DomainError()
 data class NotIndexFoundDomainError(val key: String) : DomainError()
-
